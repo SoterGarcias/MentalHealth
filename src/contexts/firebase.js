663471e47de449f402ba019920edb834f7584fb1
geanxcezar <geanxcezar@gmail.com>
2023-03-22 16:32:03 -0300

    geanCommit config firebase

---
+++ src/contexts/firebase.js
@@ -0,0 +1,20 @@
+(function (){
+// Your web app's Firebase configuration
+    // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+    var firebaseConfig = {
+      apiKey: "AIzaSyD1pPIj4oa8srwI0CHeMq4RuEF47dI4W00",
+      authDomain: "mentalhealth-be0b2.firebaseapp.com",
+      projectId: "mentalhealth-be0b2",
+      storageBucket: "mentalhealth-be0b2.appspot.com",
+      messagingSenderId: "394344862646",
+      appId: "1:394344862646:web:c46719039614132b9c2717",
+      measurementId: "G-MYXFS546Z8"
+    };
+    
+    // Initialize Firebase
+    var app = initializeApp(firebaseConfig);
+    var analytics = getAnalytics(app);
+    
+    firebase.initializeApp(firebaseConfig);
+
+})()
