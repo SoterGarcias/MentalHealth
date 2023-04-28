0f9dc965952ef16297aa626818ab6f3c5b902b9e
Soter Garcias <soter_garcia@hotmail.com>
2023-04-28 20:49:37 -0300

    Alterações no dashboard

--- src/components/dashboard/budget.js
+++ src/components/dashboard/budget.js
@@ -2,12 +2,29 @@ import { Avatar, Box, Card, CardContent, Grid, Typography } from '@mui/material'
 import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 import MoneyIcon from '@mui/icons-material/Money';
 
+
 export const Budget = (props) => (
-  <Card
-    sx={{ height: '100%' }}
-    {...props}
-  >
-    <CardContent>
+ 
+      <Card
+        sx={{ height: '100%' }}
+        {...props}
+      >
+        <Box sx={{ position: 'relative', height: '100%', display: 'flex', flexDirection: 'column' }}>
+          <Box sx={{ flex: '1 0 calc(100% - 10px)', position: 'relative' }}>
+            <img src="/static/images/dashboard/psicologo.jpg" alt="Psicologo" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+          </Box>
+          <Box sx={{ position: 'absolute', bottom: 0, left: 0, width: '100%', height: '30px', backgroundColor: 'rgba(1, 167, 104, 0.4)', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+            <Typography variant="subtitle1" sx={{ color: '#FFF', fontWeight: 'bold' }}>Psicólogos</Typography>
+          </Box>
+        </Box>
+      </Card>
+ 
+);
+
+
+
+
+{/* <CardContent>
       <Grid
         container
         spacing={3}
@@ -64,6 +81,4 @@ export const Budget = (props) => (
           Since last month
         </Typography>
       </Box>
-    </CardContent>
-  </Card>
-);
+    </CardContent> */}
