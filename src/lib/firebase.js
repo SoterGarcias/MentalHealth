363f2a74263c2f00002488e63674cc78c9e24e9f
SoterGarcias <soter_garcia@hotmail.com>
2023-05-20 15:29:02 -0300

    Ajuste de employees

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,28 +1,44 @@
 // Importe as funções que você precisa dos SDKs que você precisa
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore";
+import { getDocs } from 'firebase/firestore/lite';
+import { getFirestore, collection } from 'firebase/firestore/lite';
 
 // A configuração do Firebase do seu aplicativo da web
 // Para Firebase JS SDK v7.20.0 e posterior, measurementId é opcional
 const firebaseConfig = {
-  apiKey: "AIzaSyCt18u3LrGJU2sudwUDOy2Kbs7XQCdw74M",
-  authDomain: "mentalhealth-5232d.firebaseapp.com",
-  projectId: "mentalhealth-5232d",
-  storageBucket: "mentalhealth-5232d.appspot.com",
-  messagingSenderId: "639645757072",
-  appId: "1:639645757072:web:21255982a556ae1ceeb215",
-  measurementId: "G-9GSBZ9TT3Y"
+    apiKey: "AIzaSyCt18u3LrGJU2sudwUDOy2Kbs7XQCdw74M",
+    authDomain: "mentalhealth-5232d.firebaseapp.com",
+    projectId: "mentalhealth-5232d",
+    storageBucket: "mentalhealth-5232d.appspot.com",
+    messagingSenderId: "639645757072",
+    appId: "1:639645757072:web:21255982a556ae1ceeb215",
+    measurementId: "G-9GSBZ9TT3Y"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 let analytics = null;
-
-if (typeof window !== "undefined") {
-  analytics = getAnalytics(app);
-}
-
 const db = getFirestore(app);
 
-export { db, analytics };
+if (typeof window !== "undefined") {
+    analytics = getAnalytics(app);
+}
+
+const handleSignIn = async (email, password) => {
+    try {
+        const psicologosRef = collection(db, 'psicologos'); // Referência à coleção 'psicologos'
+        const querySnapshot = await getDocs(psicologosRef); // Obter todos os documentos da coleção 'psicologos'
+
+        querySnapshot.forEach((doc) => {
+            const userData = doc.data(); // Dados do documento
+
+            console.log('Email:', userData.email);
+            console.log('Senha:', userData.password);
+        });
+    } catch (error) {
+        console.error('Erro ao recuperar dados:', error);
+    }
+};
+
+export { db, analytics, handleSignIn };
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,36 +1,68 @@
+import React, { useState } from 'react';
 import Head from 'next/head';
 import NextLink from 'next/link';
-import Router from 'next/router';
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import { Box, Button, Container, Grid, Link, TextField, Typography } from '@mui/material';
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import { Facebook as FacebookIcon } from '../icons/facebook';
-import { Google as GoogleIcon } from '../icons/google';
+import { handleSignIn } from '../lib/firebase';
+import { db } from '../firebase';
+import { collection } from 'firebase/firestore';
+
 
 const Login = () => {
+  const [password, setPassword] = useState('');
+  const [formSubmitted, setFormSubmitted] = useState(false);
+
   const formik = useFormik({
     initialValues: {
-      email: 'teste@teste.com',
-      password: '123'
+      email: '',
+      password: ''
     },
     validationSchema: Yup.object({
-      email: Yup
-        .string()
+      email: Yup.string()
         .email('Must be a valid email')
         .max(255)
         .required('Email is required'),
-      password: Yup
-        .string()
+      password: Yup.string()
         .max(255)
         .required('Password is required')
     }),
-    onSubmit: () => {
-      Router
-        .push('/')
-        .catch(console.error);
+    onSubmit: async (values) => {
+      setFormSubmitted(true);
+      await handleSignIn(values.email, values.password);
     }
   });
+
+  const handleSignIn = async (email, password) => {
+    try {
+      const psicologosRef = db.collection('psicologos');
+      const snapshot = await psicologosRef.where('email', '==', email).get();
+  
+      if (!snapshot.empty) {
+        const userData = snapshot.docs[0].data();
+        const userPassword = userData.password;
+        console.log('Senha do usuário encontrado no Firebase:', userPassword);
+        setPassword(userPassword);
+  
+        // Verifique se a senha digitada corresponde à senha do usuário
+        if (password === userPassword) {
+          // Senha correta, faça o login do usuário
+          console.log('Senha correta. Faça o login do usuário.');
+        } else {
+          // Senha incorreta, exiba uma mensagem de erro
+          console.log('Senha incorreta. Exiba uma mensagem de erro.');
+        }
+      } else {
+        setPassword('');
+        // Usuário não encontrado, exiba uma mensagem de erro
+        console.log('Usuário não encontrado. Exiba uma mensagem de erro.');
+      }
+    } catch (error) {
+      console.error('Error retrieving user data:', error);
+      // Exiba uma mensagem de erro genérica
+      console.log('Erro ao recuperar dados do usuário.', error);
+    }
+  };
 
   return (
     <>
@@ -38,184 +70,139 @@ const Login = () => {
         <title>Login | Mental Health</title>
       </Head>
       <div style={{ display: 'flex' }}>
-      <Box
-        sx={{
-          display: "flex",
-          // alignItems: "center",
-          // justifyContent: "center",
-          height: "100vh",
-          backgroundColor: "#009099",
-          flex: 1
-        }}
-      >
-        <Typography variant="h1" 
-        sx={{ 
-          color: "white", 
-          textAlign: "left",
-          px: 15,
-          py: 10,
-          }}>
-          Mental <br /> Healt
-        </Typography>
-        <img src="/logo.svg" alt="logo" />
-      </Box>
-      <Box
-        component="main"
-        sx={{
-          justifyContent: "flex-end",
-          alignItems: "center",
-          display: "flex",
-          flexGrow: 1,
-          minHeight: "100%",
-          height: "100vh",
-          maxWidth: "400px",
-          backgroundColor: "#283342",
-          color: "white",
-          px: 3,
-
-        }}
-      >
-        <Container maxWidth="sm">
-          {/* <NextLink
-            href="/"
-            passHref
+        <Box
+          sx={{
+            display: 'flex',
+            height: '100vh',
+            backgroundColor: '#009099',
+            flex: 1
+          }}
+        >
+          <Typography
+            variant="h1"
+            sx={{
+              color: 'white',
+              textAlign: 'left',
+              px: 15,
+              py: 10
+            }}
           >
-            <Button
-              component="a"
-              startIcon={<ArrowBackIcon fontSize="small" />}
-            >
-              Dashboard
-            </Button>
-          </NextLink> */}
-          <form onSubmit={formik.handleSubmit}>
-            <Box sx={{ my: 3 }}>
-              <Typography
-                color="white"
-                variant="h4"
-              >
-                Bem vindo de volta! Fico feliz em vê-lo, novamente!
-              </Typography>
-              {/* <Typography
-                color="white"
-                gutterBottom
-                variant="body2"
-              >
-                Sign in on the internal platform
-              </Typography> */}
-            </Box>
-            <Grid
-              container
-              spacing={3}
-            >
-              <Grid
-                item
-                xs={12}
-                md={6}
-              >
-                <Button
-                  color="info"
-                  fullWidth
-                  startIcon={<FacebookIcon />}
-                  onClick={() => formik.handleSubmit()}
-                  size="small"
-                  variant="contained"
-                >
-                  Login com Facebook
-                </Button>
+            Mental <br /> Health
+          </Typography>
+          <img src="/logo.svg" alt="logo" />
+        </Box>
+        <Box
+          component="main"
+          sx={{
+            justifyContent: 'flex-end',
+            alignItems: 'center',
+            display: 'flex',
+            flexGrow: 1,
+            minHeight: '100%',
+            height: '100vh',
+            maxWidth: '400px',
+            backgroundColor: '#283342',
+            color: 'white',
+            px: 3
+          }}
+        >
+          <Container maxWidth="sm">
+            <form onSubmit={formik.handleSubmit}>
+              <Box sx={{ my: 3 }}>
+                <Typography color="white" variant="h4">
+                  Bem vindo de volta! Fico feliz em vê-lo, novamente!
+                </Typography>
+              </Box>
+              <Grid container spacing={3}>
+                <Grid item xs={12} md={6}>
+                  <Button
+                    color="info"
+                    fullWidth
+                    size="small"
+                    variant="contained"
+                  >
+                    Login com Facebook
+                  </Button>
+                </Grid>
+                <Grid item xs={12} md={6}>
+                  <Button
+                    color="error"
+                    fullWidth
+                    size="small"
+                    variant="contained"
+                  >
+                    Login com Google
+                  </Button>
+                </Grid>
               </Grid>
-              <Grid
-                item
-                xs={12}
-                md={6}
-              >
-                <Button
-                  color="error"
-                  fullWidth
-                  onClick={() => formik.handleSubmit()}
-                  size="small"
-                  startIcon={<GoogleIcon />}
-                  variant="contained"
+              <Box sx={{ pb: 1, pt: 3 }}>
+                <Typography
+                  align="center"
+                  color="textSecondary"
+                  variant="body1"
                 >
-                  Login com Google
-                </Button>
-              </Grid>
-            </Grid>
-            <Box
-              sx={{
-                pb: 1,
-                pt: 3
-              }}
-            >
-              <Typography
-                align="center"
-                color="textSecondary"
-                variant="body1"
-              >
-                ou faça o login com endereço de e-mail
-              </Typography>
-            </Box>
-            <TextField
-              error={Boolean(formik.touched.email && formik.errors.email)}
-              fullWidth
-              helperText={formik.touched.email && formik.errors.email}
-              label="Email"
-              margin="normal"
-              name="email"
-              onBlur={formik.handleBlur}
-              onChange={formik.handleChange}
-              type="email"
-              value={formik.values.email}
-              variant="outlined"
-            />
-            <TextField
-              error={Boolean(formik.touched.password && formik.errors.password)}
-              fullWidth
-              helperText={formik.touched.password && formik.errors.password}
-              label="Password"
-              margin="normal"
-              name="password"
-              onBlur={formik.handleBlur}
-              onChange={formik.handleChange}
-              type="password"
-              value={formik.values.password}
-              variant="outlined"
-            />
-            <Box sx={{ py: 2 }}>
-              <Button
-                color="primary"
-                disabled={formik.isSubmitting}
+                  ou faça o login com endereço de e-mail
+                </Typography>
+              </Box>
+              <TextField
+                inputProps={{ style: { color: 'white' } }}
+                error={formik.touched.email && Boolean(formik.errors.email)}
                 fullWidth
-                size="large"
-                type="submit"
-                variant="contained"
-              >
-                Sign In Now
-              </Button>
-            </Box>
-            <Typography
-              color="textSecondary"
-              variant="body2"
-            >
-              Don&apos;t have an account?
-              {' '}
-              <NextLink
-                href="/register"
-              >
-                <Link
-                  to="/register"
-                  variant="subtitle2"
-                  underline="hover"
-                  sx={{
-                    cursor: 'pointer'
-                  }}
+                helperText={formik.touched.email && formik.errors.email}
+                label="Email"
+                margin="normal"
+                name="email"
+                onBlur={formik.handleBlur}
+                onChange={formik.handleChange}
+                type="email"
+                value={formik.values.email}
+                variant="outlined"
+              />
+              <TextField
+                inputProps={{ style: { color: 'white' } }}
+                error={formik.touched.password && Boolean(formik.errors.password)}
+                fullWidth
+                helperText={formik.touched.password && formik.errors.password}
+                label="Password"
+                margin="normal"
+                name="password"
+                onBlur={formik.handleBlur}
+                onChange={formik.handleChange}
+                type="password"
+                value={formik.values.password}
+                variant="outlined"
+              />
+              <Box sx={{ py: 2 }}>
+                <Button
+                  color="primary"
+                  disabled={formik.isSubmitting}
+                  fullWidth
+                  size="large"
+                  type="submit"
+                  variant="contained"
                 >
-                  Sign Up
-                </Link>
-              </NextLink>
-            </Typography>
-          </form>
-        </Container>
-      </Box>
+                  Sign In Now
+                </Button>
+              </Box>
+              <Typography color="textSecondary" variant="body2">
+                Don&apos;t have an account?
+                {' '}
+                <NextLink href="/register">
+                  <Link
+                    to="/register"
+                    variant="subtitle2"
+                    underline="hover"
+                    sx={{
+                      cursor: 'pointer'
+                    }}
+                  >
+                    Sign Up
+                  </Link>
+                </NextLink>
+              </Typography>
+            </form>
+          </Container>
+        </Box>
       </div>
     </>
   );
