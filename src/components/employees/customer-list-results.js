a2bce5528c1b67c3ff50117579a76a268a35db5b
SoterGarcias <soter_garcia@hotmail.com>
2023-05-26 17:28:56 -0300

    foto na lista geral

--- src/components/employees/customer-list-results.js
+++ src/components/employees/customer-list-results.js
@@ -2,19 +2,7 @@ import { useState } from 'react';
 import PerfectScrollbar from 'react-perfect-scrollbar';
 import PropTypes from 'prop-types';
 import { format } from 'date-fns';
-import {
-  Avatar,
-  Box,
-  Card,
-  Checkbox,
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TablePagination,
-  TableRow,
-  Typography
-} from '@mui/material';
+import {Avatar,  Box, Card, Checkbox, Table, TableBody, TableCell, TableHead, TablePagination, TableRow, Typography } from '@mui/material';
 import { getInitials } from '../../utils/get-initials';
 
 export const CustomerListResults = ({ customers, ...rest }) => {
@@ -122,7 +110,7 @@ export const CustomerListResults = ({ customers, ...rest }) => {
                       }}
                     >
                       <Avatar
-                        src={customer.avatarUrl}
+                        src={customer.profileImageUrl}
                         sx={{ mr: 2 }}
                       >
                         {getInitials(customer.name)}
