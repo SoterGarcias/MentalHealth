34656aa1b1dc829421962b1f6208e7de1b5b77f1
SoterGarcias <soter_garcia@hotmail.com>
2023-06-08 07:47:43 -0300

    apresentação psicologo

--- src/pages/psicologos.js
+++ src/pages/psicologos.js
@@ -1,89 +1,175 @@
 import Head from 'next/head';
-import { Box, Container, Grid, Pagination } from '@mui/material';
-import { products } from '../__mocks__/products';
-import  ProductListToolbar  from '../components/psicologos/product-list-toolbar';
-import ProductCard from '../components/psicologos/product-card';
-import { DashboardLayout } from '../components/dashboard-layout';
-import Link from 'next/link';
-import { useRouter } from "next/router";
-import { getDocs, collection } from 'firebase/firestore/lite';
-import { db } from '../lib/firebase';
-import { useState, useEffect } from 'react';
+// import ArrowUpOnSquareIcon from '@heroicons/react/24/solid/ArrowUpOnSquareIcon';
+// import ArrowDownOnSquareIcon from '@heroicons/react/24/solid/ArrowDownOnSquareIcon';
+// import PlusIcon from '@heroicons/react/24/solid/PlusIcon';
+import {
+  Box,
+  Button,
+  Container,
+  Pagination,
+  Stack,
+  SvgIcon,
+  Typography,
+  Unstable_Grid2 as Grid
+} from '@mui/material';
+// 'src/layouts/dashboard/layout'
+import { Layout as DashboardLayout } from '../components/settings/';
+import { CompanyCard } from 'src/sections/companies/company-card';
+import { CompaniesSearch } from 'src/sections/companies/companies-search';
 
+const companies = [
+  {
+    id: '2569ce0d517a7f06d3ea1f24',
+    createdAt: '27/03/2019',
+    description: 'Dropbox is a file hosting service that offers cloud storage, file synchronization, a personal cloud.',
+    logo: '/assets/logos/logo-dropbox.png',
+    title: 'Dropbox',
+    downloads: '594'
+  },
+  {
+    id: 'ed2b900870ceba72d203ec15',
+    createdAt: '31/03/2019',
+    description: 'Medium is an online publishing platform developed by Evan Williams, and launched in August 2012.',
+    logo: '/assets/logos/logo-medium.png',
+    title: 'Medium Corporation',
+    downloads: '625'
+  },
+  {
+    id: 'a033e38768c82fca90df3db7',
+    createdAt: '03/04/2019',
+    description: 'Slack is a cloud-based set of team collaboration tools and services, founded by Stewart Butterfield.',
+    logo: '/assets/logos/logo-slack.png',
+    title: 'Slack',
+    downloads: '857'
+  },
+  {
+    id: '1efecb2bf6a51def9869ab0f',
+    createdAt: '04/04/2019',
+    description: 'Lyft is an on-demand transportation company based in San Francisco, California.',
+    logo: '/assets/logos/logo-lyft.png',
+    title: 'Lyft',
+    downloads: '406'
+  },
+  {
+    id: '1ed68149f65fbc6089b5fd07',
+    createdAt: '04/04/2019',
+    description: 'GitHub is a web-based hosting service for version control of code using Git.',
+    logo: '/assets/logos/logo-github.png',
+    title: 'GitHub',
+    downloads: '835'
+  },
+  {
+    id: '5dab321376eff6177407e887',
+    createdAt: '04/04/2019',
+    description: 'Squarespace provides software as a service for website building and hosting. Headquartered in NYC.',
+    logo: '/assets/logos/logo-squarespace.png',
+    title: 'Squarespace',
+    downloads: '835'
+  }
+];
 
-console.log("Verifying Page component"); // Adicione esta linha
-const Psicologos = () => {
-  const [product, setProduct] = useState(null);
-  const [error, setError] = useState(null); // Adicione esta linha
-
-  useEffect(() => {
-    console.log('Iniciando busca do produto...');
-    const fetchProduct = async () => {
-      try {
-        const productRef = collection(db, 'psicologos');
-        const querySnapshot = await getDocs(productRef);
-
-        const productsData = querySnapshot.docs.map((doc) => {
-          const data = doc.data();
-          data.id = doc.id;
-          return data;
-        });
-        setProduct(productsData);
-      } catch (error) {
-        setError('Erro ao buscar os produtos.');
-      }
-    };
-
-    fetchProduct();
-  }, []);
-
-  console.log("Inside Page component"); // Adicione esta linha
-  const router = useRouter();
-  console.log("Router initialized", router); // Adicione esta linha
-
-  return (
-    <>
-      <Head>
-        <title>Psicólogos | Mental Health</title>
-      </Head>
-      <Box
-        component="main"
-        sx={{
-          flexGrow: 1,
-          py: 8,
-        }}
-      >
-        <Container maxWidth={false}>
-          <ProductListToolbar />
-
-          <Box sx={{ p: 3 }}>
-            <Grid container spacing={3}>
-              {product?.map((productItem) => {
-                console.log('Product Item:', productItem);
-                return (
-                  <Link key={productItem.id} href={`/psicologos/${productItem.id}`}>
-                    <Grid item lg={4} md={4} sm={6} xs={12}>
-                      <ProductCard product={productItem} id={productItem.id} />
-                    </Grid>
-                  </Link>);
-              })}
-            </Grid>
-          </Box>
+const Page = () => (
+  <>
+    <Head>
+      <title>
+        Companies | Devias Kit
+      </title>
+    </Head>
+    <Box
+      component="main"
+      sx={{
+        flexGrow: 1,
+        py: 8
+      }}
+    >
+      <Container maxWidth="xl">
+        <Stack spacing={3}>
+          <Stack
+            direction="row"
+            justifyContent="space-between"
+            spacing={4}
+          >
+            <Stack spacing={1}>
+              <Typography variant="h4">
+                Companies
+              </Typography>
+              <Stack
+                alignItems="center"
+                direction="row"
+                spacing={1}
+              >
+                <Button
+                  color="inherit"
+                  startIcon={(
+                    <SvgIcon fontSize="small">
+                      {/* <ArrowUpOnSquareIcon /> */}
+                    </SvgIcon>
+                  )}
+                >
+                  Import
+                </Button>
+                <Button
+                  color="inherit"
+                  startIcon={(
+                    <SvgIcon fontSize="small">
+                      {/* <ArrowDownOnSquareIcon /> */}
+                    </SvgIcon>
+                  )}
+                >
+                  Export
+                </Button>
+              </Stack>
+            </Stack>
+            <div>
+              <Button
+                startIcon={(
+                  <SvgIcon fontSize="small">
+                    {/* <PlusIcon /> */}
+                  </SvgIcon>
+                )}
+                variant="contained"
+              >
+                Add
+              </Button>
+            </div>
+          </Stack>
+          <CompaniesSearch />
+          <Grid
+            container
+            spacing={3}
+          >
+            {companies.map((company) => (
+              <Grid
+                xs={12}
+                md={6}
+                lg={4}
+                key={company.id}
+              >
+                <CompanyCard company={company} />
+              </Grid>
+            ))}
+          </Grid>
           <Box
             sx={{
-              display: "flex",
-              justifyContent: "center",
-              pt: 3,
+              display: 'flex',
+              justifyContent: 'center'
             }}
           >
-            <Pagination color="primary" count={3} size="small" />
+            <Pagination
+              count={3}
+              size="small"
+            />
           </Box>
-        </Container>
-      </Box>
-    </>
-  );
-};
+        </Stack>
+      </Container>
+    </Box>
+  </>
+);
 
-Psicologos.getLayout = (page) => <DashboardLayout>{page}</DashboardLayout>;
+Page.getLayout = (page) => (
+  <DashboardLayout>
+    {page}
+  </DashboardLayout>
+);
 
-export default Psicologos;
+export default Page;
