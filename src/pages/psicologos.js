0f8586066b569dc3d184f2a6f4e3199b564c3bed
SoterGarcias <soter_garcia@hotmail.com>
2023-06-11 01:16:00 -0300

    pagina do psicologo

--- src/pages/psicologos.js
+++ src/pages/psicologos.js
@@ -6,11 +6,11 @@ import { ProductCard } from '../components/psicologos/product-card';
 import { DashboardLayout } from '../components/dashboard-layout';
 import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
 import { db } from '../../src/lib/firebase';
-
+import Link from 'next/link';
 
 const Page = () => {
   const [loadedProducts, setLoadedProducts] = useState([]);
-  
+
   useEffect(() => {
     const loadProducts = async () => {
       try {
@@ -31,58 +31,66 @@ const Page = () => {
 
     loadProducts();
   }, []);
-  
+
   return (
-  <>
-    <Head>
-      <title>
-        Products | Material Kit
-      </title>
-    </Head>
-    <Box
-      component="main"
-      sx={{
-        flexGrow: 1,
-        py: 8
-      }}
-    >
-      <Container maxWidth={false}>
-        <ProductListToolbar />
-        <Box sx={{ pt: 3 }}>
-          <Grid
-            container
-            spacing={3}
+    <>
+      <Head>
+        <title>
+          Products | Material Kit
+        </title>
+      </Head>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          py: 8
+        }}
+      >
+        <Container maxWidth={false}>
+          <ProductListToolbar />
+          <Box sx={{ pt: 3 }}>
+            <Grid
+              container
+              spacing={3}
+            >
+              {loadedProducts.map((product) => (
+                product.typeUser === '' ? null : (
+
+                  <Grid
+                    item
+                    key={product.id}
+                    lg={4}
+                    md={6}
+                    xs={12}
+                  >
+                    {/* <link href={'/psicologos/${product.id}'}> */}
+                      <a>
+                        <ProductCard product={product} />
+                      </a>
+                    {/* </link> */}
+                  </Grid>
+
+                )
+              ))}
+            </Grid>
+          </Box>
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              pt: 3
+            }}
           >
-            {loadedProducts.map((product) => (
-              <Grid
-                item
-                key={product.id}
-                lg={4}
-                md={6}
-                xs={12}
-              >
-                <ProductCard product={product} />
-              </Grid>
-            ))}
-          </Grid>
-        </Box>
-        <Box
-          sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            pt: 3
-          }}
-        >
-          <Pagination
-            color="primary"
-            count={3}
-            size="small"
-          />
-        </Box>
-      </Container>
-    </Box>
-  </>
-);
+            <Pagination
+              color="primary"
+              count={3}
+              size="small"
+            />
+          </Box>
+        </Container>
+      </Box>
+    </>
+  );
 };
 
 Page.getLayout = (page) => (
